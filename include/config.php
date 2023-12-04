4ff7a8f998d915c3d4529d8b8da885cf5742666e
donaldtchana <donaldtchana@gmail.com>
2023-12-04 08:38:13 +0100

    verification

--- include/config.php
+++ include/config.php
@@ -1,9 +1,9 @@
 <?php
 @define("HOST_NAME", "localhost");
 @define("DB_NAME", "demo");
-@define("USER_NAME", "root");
-@define("USER_PASSWORD", "12345");
-@define('DB_PORT', '3306');
+@define("USER_NAME", "administrator");
+@define("USER_PASSWORD", "system");
+@define('DB_PORT', '5785');
 @define("MAX_UPLOAD_SIZE", 50);
 @define("SITE_URL", "http://localhost/Iplans");
 @define("COURRIER_API_URL", "http://localhost/courrier/");
@@ -11,3 +11,4 @@
 @define("MISSION_API_URL", "http://localhost/mission/");
 @define("PERMISSION_API_URL", "http://localhost/permission/");
 @define("siteiplans_API_URL", "http://localhost/siteiplans/");
+@define("FRAIS_MISSION_API_URL", "http://localhost/fraismission/");
